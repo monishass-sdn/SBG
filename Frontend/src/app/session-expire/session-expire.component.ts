2976a95c209588e36cb30313ea9f253d75d91ac3
ashly1234 <94604548+ashly1234@users.noreply.github.com>
2022-02-28 12:20:01 +0530

    Add files via upload

--- Frontend/src/app/session-expire/session-expire.component.ts
+++ Frontend/src/app/session-expire/session-expire.component.ts
@@ -1,1 +1,15 @@
+import { Component, OnInit } from '@angular/core';
 
+@Component({
+  selector: 'app-session-expire',
+  templateUrl: './session-expire.component.html',
+  styleUrls: ['./session-expire.component.css']
+})
+export class SessionExpireComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
