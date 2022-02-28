84b58806186d4ff73d86141e58054a52dc899a0d
ashly1234 <94604548+ashly1234@users.noreply.github.com>
2022-02-28 12:15:33 +0530

    Add files via upload

--- Frontend/src/app/owner-header/owner-header.component.ts
+++ Frontend/src/app/owner-header/owner-header.component.ts
@@ -1,1 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+declare var $: any;
+declare var jQuery: any;
+@Component({
+  selector: 'app-owner-header',
+  templateUrl: './owner-header.component.html',
+  styleUrls: ['./owner-header.component.css']
+})
+export class OwnerHeaderComponent implements OnInit {
 
+  constructor() { }
+
+  ngOnInit(): void {
+
+    $(document).ready(function(){
+
+      $(".mobile-menu-icon").click(function(){
+      $(".side-menu").toggleClass("mobile-sidebar");
+    });
+  
+  });
+
+     $(".mobile-menu-icon").click(function(){
+       $(".side-menu").toggleClass("mobile-sidebar");
+     });
+
+  }
+  logout(){
+    sessionStorage.clear();
+    //sessionStorage.setItem('userlogin', JSON.stringify(false));   // if it's object
+
+  }
+  Help(){
+    window.open('https://smartboating.com.au/booking-system-user-guide/'); 
+  }
+}
