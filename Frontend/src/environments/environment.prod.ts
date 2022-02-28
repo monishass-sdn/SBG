a7e383aca894b467c899d1787403c48eeae27f72
ashly1234 <94604548+ashly1234@users.noreply.github.com>
2022-02-28 11:24:24 +0530

    Add files via upload

---
+++ Frontend/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  timeZone:'Australia/Sydney',
+  // Add base URL for environment Done By Alagesan	on 06.07.2021
+  url: 'http://65.2.28.16/'
+  //url: 'https://bookings.smartboating.com.au/'
+ // url: 'https://test.smartboating.com.au/'
+  
+
+
+};
+
+
---
+++ Frontend/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+
+  timeZone:'Australia/Sydney',
+  
+  // Add base URL for environment Done By Alagesan	on 06.07.2021
+  url: 'http://65.2.28.16/'
+  //url: 'https://bookings.smartboating.com.au/'
+  //url: 'https://test.smartboating.com.au/' 
+ 
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
